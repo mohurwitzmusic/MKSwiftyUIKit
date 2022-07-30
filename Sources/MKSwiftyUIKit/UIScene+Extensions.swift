5cb2b9acb6605960605609d8d4b0ac915a40c461
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2022-07-30 12:23:41 -0700

    Add UIScene+Extensions

---
+++ Sources/MKSwiftyUIKit/UIScene+Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by MOH on 2022-07-30.
+//
+
+import UIKit
+
+
+extension UIScene {
+    
+    func configureWindow(rootViewController: UIViewController, embedInNavigationController: Bool, toolbarHidden: Bool) -> UIWindow! {
+        if let windowScene = self as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let initialViewController: UIViewController
+            if embedInNavigationController {
+                let navigationController = UINavigationController()
+                navigationController.setToolbarHidden(false, animated: false)
+                navigationController.viewControllers = [rootViewController]
+                initialViewController = navigationController
+            } else {
+                initialViewController = rootViewController
+            }
+            window.rootViewController = initialViewController
+            return window
+        }
+        return nil
+    }
+    
+}
