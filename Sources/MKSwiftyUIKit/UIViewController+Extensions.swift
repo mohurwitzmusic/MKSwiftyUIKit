437a1a936ed986162629121e88d4be43194b69a4
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2022-08-04 19:40:51 -0700

    rename addChild

--- Sources/MKSwiftyUIKit/UIViewController+Extensions.swift
+++ Sources/MKSwiftyUIKit/UIViewController+Extensions.swift
@@ -9,7 +9,7 @@ import UIKit
 
 public extension UIViewController {
 
-    func quickAddChildAndView(_ viewController: UIViewController, frame: CGRect?) {
+    func addChildAndView(_ viewController: UIViewController, frame: CGRect?) {
         viewController.willMove(toParent: self)
         addChild(viewController)
         view.addSubview(viewController.view)
