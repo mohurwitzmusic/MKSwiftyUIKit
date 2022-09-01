6e1c868b6340b46d1f84520137e81f3bf8ed8468
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2022-09-01 12:49:17 -0700

    Add CGRect Extensions

---
+++ Sources/MKSwiftyUIKit/CGRect+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by MOH on 2022-09-01.
+//
+
+import UIKit
+
+public extension CGRect {
+    
+    var center: CGPoint {
+        return .init(x: midX, y: midY)
+    }
+    
+}
