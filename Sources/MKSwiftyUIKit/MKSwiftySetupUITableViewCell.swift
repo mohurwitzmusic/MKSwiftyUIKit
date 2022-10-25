17f455e64c618474955ce616ef862368a4b5eb9a
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2022-10-24 19:47:04 -0700

    Add MKSwiftySetupUITableViewCell

---
+++ Sources/MKSwiftyUIKit/MKSwiftySetupUITableViewCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+open class MKSwiftySetupUITableViewCell: UITableViewCell {
+    
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    open func setup() { }
+    
+}
