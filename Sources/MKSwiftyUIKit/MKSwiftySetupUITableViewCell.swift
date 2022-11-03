5fff92e60df9650b04ff4881fa3f9bfcb40a04d0
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2022-11-02 19:55:55 -0700

    Add MKSwiftySetupUITextField and new features to MKSwiftySetupUITableViewCell

--- Sources/MKSwiftyUIKit/MKSwiftySetupUITableViewCell.swift
+++ Sources/MKSwiftyUIKit/MKSwiftySetupUITableViewCell.swift
@@ -2,6 +2,34 @@ import UIKit
 
 open class MKSwiftySetupUITableViewCell: UITableViewCell {
     
+    private var didConfigure = false
+    
+    /// Use to define the width of an accessory. `layoutSubviews` checks
+    /// to see if an `accessoryView` is set and uses `accessoryWidth` to
+    /// calculate its size. The view will be centered vertically.
+    
+    public struct AccessoryWidth: Equatable, Sendable {
+        public var max: CGFloat?
+        public var size = Size.fractional(0.5)
+        public enum Size: Equatable, Sendable {
+            case fractional(CGFloat)
+            case fixed(CGFloat)
+        }
+        public init(size: Size = Size.fractional(0.5), max: CGFloat? = nil) {
+            self.max = max
+            self.size = size
+        }
+    }
+    
+    /// The width of the accessory to use during `layoutSubviews`. Setting this
+    /// casuses `layoutSubviews` to be called.
+    
+    open var accessoryWidth = AccessoryWidth() {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
     public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setup()
@@ -12,6 +40,46 @@ open class MKSwiftySetupUITableViewCell: UITableViewCell {
         setup()
     }
     
+    /// Override to perform any setup after initialization.
+    
     open func setup() { }
     
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutAccessory()
+    }
+    
+    /// Use to provide an initial configuration. This is only called once.
+    
+    public final func configureOnce(_ configure: @escaping () -> ()) {
+        if didConfigure { return }
+        didConfigure = true
+        configure()
+    }
+    
+    /// Lays out the `accessoryView`. Called in `layoutSubviews`.
+    
+    open func layoutAccessory() {
+        guard let accessoryView else { return }
+        var width: CGFloat
+        switch accessoryWidth.size {
+        case .fixed(let cGFloat):
+            width = cGFloat
+        case .fractional(let cGFloat):
+            width = bounds.width * cGFloat
+        }
+        if let max = accessoryWidth.max {
+            width = min(max, width)
+        }
+        let maxWidth = bounds.width - directionalLayoutMargins.leading - directionalLayoutMargins.trailing
+        width = min(maxWidth, width)
+        let height = accessoryView.sizeThatFits(accessoryView.frame.size).height
+        accessoryView.frame = .init(x: bounds.maxX - width - directionalLayoutMargins.leading,
+                                  y: bounds.midY - height/2,
+                                 width: width,
+                                 height: height)
+    }
+    
 }
+
+
---
+++ Sources/MKSwiftyUIKit/MKSwiftySetupUITextField.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+/// Adds itself as its delegate and uses closures for callbacks.
+
+open class MKSwiftySetupUITextField: UITextField, UITextFieldDelegate {
+    
+    open var shouldReturn: ((UITextField) -> Bool)?
+    open var didEndEditing: ((UITextField) -> Void)?
+    open var shouldEndEditing: ((UITextField) -> Bool)?
+    open var shouldChangeCharactersInRange: ((UITextField, NSRange, String) -> Bool)?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    open func setup() {
+        delegate = self
+    }
+    
+    open func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return self.shouldReturn?(self) ?? true
+    }
+    
+    open func textFieldDidEndEditing(_ textField: UITextField) {
+        didEndEditing?(self)
+    }
+    
+    open func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return self.shouldEndEditing?(self) ?? true
+    }
+    
+    open func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return self.shouldChangeCharactersInRange?(self, range, string) ?? true
+    }
+    
+    
+}
+
+public extension MKSwiftySetupUITextField {
+    
+    @discardableResult
+    func onShouldReturn( _ handler: @escaping ((UITextField) -> Bool)) -> Self {
+        self.shouldReturn = handler
+        return self
+    }
+    
+    @discardableResult
+    func onShouldEndEditing( _ handler: @escaping ((UITextField) -> Bool)) -> Self {
+        self.shouldEndEditing = handler
+        return self
+    }
+    
+    @discardableResult
+    func onDidEndEditing(_ handler: @escaping ((UITextField) -> Void)) -> Self {
+        self.didEndEditing = handler
+        return self
+    }
+    
+    @discardableResult
+    func onShouldChangeCharactersInRange(_ handler: @escaping ((UITextField, NSRange, String) -> Bool)) -> Self {
+        self.shouldChangeCharactersInRange = handler
+        return self
+    }
+
+}
+
+
