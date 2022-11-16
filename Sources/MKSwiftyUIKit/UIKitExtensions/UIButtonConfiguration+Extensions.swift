e8952b39112fadb8e574548ec798206d505a36b4
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2022-11-15 17:14:05 -0800

    Add fluent UIButtonConfiguration helpers

---
+++ Sources/MKSwiftyUIKit/UIKitExtensions/UIButtonConfiguration+Extensions.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+@available(iOS 15.0, *)
+public extension UIButton.Configuration {
+    
+    func withTitle(_ title: String?) -> Self {
+        var copy = self
+        copy.title = title
+        return copy
+    }
+    
+    func withSubtitle(_ subtitle: String?) -> Self {
+        var copy = self
+        copy.subtitle = subtitle
+        return copy
+    }
+    
+    func withImage(systemName: String) -> Self {
+        var copy = self
+        copy.image = .init(systemName: systemName)
+        return self
+    }
+    
+    func withBaseForegroundColor(_ color: UIColor) -> Self {
+        var copy = self
+        copy.baseForegroundColor = color
+        return copy
+    }
+    
+    func withBaseBackgroundColor(_ color: UIColor) -> Self {
+        var copy = self
+        copy.baseBackgroundColor = color
+        return copy
+    }
+
+    func withTitleAlignment(_ alignment: UIButton.Configuration.TitleAlignment) -> Self {
+        var copy = self
+        copy.titleAlignment = alignment
+        return copy
+    }
+    
+    func withImagePadding(_ padding: CGFloat) -> Self {
+        var copy = self
+        copy.imagePadding = padding
+        return copy
+    }
+    
+    func withTitlePadding(_ padding: CGFloat) -> Self {
+        var copy = self
+        copy.titlePadding = padding
+        return copy
+    }
+    
+}
