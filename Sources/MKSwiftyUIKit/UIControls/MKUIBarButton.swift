824ddc7e6af795b06b6ccb060a1002cffd3725b3
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2022-12-01 01:02:06 -0800

    Fix event handler not called

--- Sources/MKSwiftyUIKit/UIControls/MKUIBarButton.swift
+++ Sources/MKSwiftyUIKit/UIControls/MKUIBarButton.swift
@@ -3,12 +3,11 @@ import Combine
 
 open class MKUIBarButton: UIBarButtonItem {
     
-    open var eventHandler: (() -> Void)?
     open var observedObject: AnyCancellable?
     
     @discardableResult
     public func onButtonPressed<T: AnyObject>(target: T, handler: @escaping ((T) -> Void)) -> Self {
-        self.eventHandler = { [weak target] in
+        self.primaryAction = UIAction { [weak target] _ in
             guard let target else { return }
             handler(target)
         }
