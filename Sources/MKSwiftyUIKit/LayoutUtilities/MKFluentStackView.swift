be32fc56dfbcb47d95c86f125db0c9b60fd7ef92
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2023-04-08 15:39:56 -0700

    Add MKHStack and MKVStack

--- Sources/MKSwiftyUIKit/LayoutUtilities/MKFluentStackView.swift
+++ Sources/MKSwiftyUIKit/LayoutUtilities/MKFluentStackView.swift
@@ -40,3 +40,52 @@ open class MKFluentStackView: UIStackView {
     }
 }
 
+
+open class MKHStack: MKFluentStackView {
+    
+    public init(distribution: UIStackView.Distribution = .fill, alignment: UIStackView.Alignment = .fill, spacing: CGFloat = 0, @ViewBuilder content: () -> [UIView]) {
+        super.init(axis: .horizontal, distribution: distribution, alignment: alignment, spacing: spacing, content: content)
+    }
+    
+    public required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+open class MKVStack: MKFluentStackView {
+    
+    public init(distribution: UIStackView.Distribution = .fill, alignment: UIStackView.Alignment = .fill, spacing: CGFloat = 0, @ViewBuilder content: () -> [UIView]) {
+        super.init(axis: .vertical, distribution: distribution, alignment: alignment, spacing: spacing, content: content)
+    }
+    
+    public required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+public extension MKFluentStackView {
+    
+    @discardableResult
+      func spacing(_ value: CGFloat) -> Self {
+          self.spacing = value
+          
+          func updateSpacing(for view: UIView) {
+              if let stackView = view as? MKFluentStackView {
+                  stackView.spacing = value
+              }
+              view.subviews.forEach { subview in
+                  updateSpacing(for: subview)
+              }
+          }
+          
+          arrangedSubviews.forEach { subview in
+              updateSpacing(for: subview)
+          }
+          
+          return self
+      }
+    
+}
