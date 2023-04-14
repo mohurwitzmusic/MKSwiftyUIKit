76936e6733e7183f55e378645e73bd96d844fc6a
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2023-04-14 12:44:14 -0700

    Add fill and stroke fluent API to UIBezierPath

--- Sources/MKSwiftyUIKit/UIKitExtensions/UIBezierPath+Extensions.swift
+++ Sources/MKSwiftyUIKit/UIKitExtensions/UIBezierPath+Extensions.swift
@@ -11,4 +11,18 @@ public extension UIBezierPath {
         return UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
     }
     
+    @discardableResult
+    func fill(color: UIColor) -> Self {
+        color.setFill()
+        fill()
+        return self
+    }
+    
+    @discardableResult
+    func stroke(color: UIColor) -> Self {
+        color.setStroke()
+        stroke()
+        return self
+    }
+    
 }
