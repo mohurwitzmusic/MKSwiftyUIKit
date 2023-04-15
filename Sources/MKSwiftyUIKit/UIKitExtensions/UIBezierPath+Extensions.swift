56409e14dea1f1b503ec427a42c89bdf5762ad19
MIchael O. Hurwitz <mohurwitzmusic@gmail.com>
2023-04-15 13:46:52 -0700

    Add fluent API for setting a UIBezierPath lineWIdth

--- Sources/MKSwiftyUIKit/UIKitExtensions/UIBezierPath+Extensions.swift
+++ Sources/MKSwiftyUIKit/UIKitExtensions/UIBezierPath+Extensions.swift
@@ -11,6 +11,8 @@ public extension UIBezierPath {
         return UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
     }
     
+    /// Fluent API for filling the path.
+    
     @discardableResult
     func fill(color: UIColor) -> Self {
         color.setFill()
@@ -18,11 +20,22 @@ public extension UIBezierPath {
         return self
     }
     
+    /// Fluent API for stroking the path.
+    
     @discardableResult
     func stroke(color: UIColor) -> Self {
         color.setStroke()
         stroke()
         return self
     }
+    
+    /// Fluent API for setting the path's `lineWidth`.
+    
+    @discardableResult
+    func setLineWidth(_ lineWidth: CGFloat) -> Self {
+        self.lineWidth = lineWidth
+        return self
+    }
+    
     
 }
